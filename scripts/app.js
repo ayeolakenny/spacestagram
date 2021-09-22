862fe3c7bb5f32954e40766f86ed5413cc13458b
Ayeola Kehinde Olatunde <55482979+ayeolakenny@users.noreply.github.com>
2021-09-22 20:52:14 +0100

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -24,39 +24,61 @@
 //         </div>`;
 //         html += htmlSegment;
 //     });
-    
+
 //     let container = document.querySelector('.row');
 //     container.innerHTML = html;
 // }
 
 // renderImages();
 
-
+// let sendRequest = () => {
+//   fetch("http://images-api.nasa.gov/search?q=apollo")
+//     .then((res) => {
+//       if (!res.ok) {
+//         throw Error("Error getting Images");
+//       }
+//       return res.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       let outputData = ``;
+//       data.collection.items.map((val) => {
+//         // the rendered card
+//         outputData += `
+//         <div class="column">
+//          <img src=${val.links} alt="Hello" />
+//         </div>
+//       `;
+//       });
+//       output.innerHTML = outputData;
+//       console.log(data.collection.items);
+//     })
+//     .catch((err) => alert(err));
+// };
 
 let output = document.querySelector(".row");
 
-let sendRequest = () =>{
-  fetch('http://images-api.nasa.gov/search?q=apollo')
-  .then(res=>{
-    if(!res.ok){
-      throw Error("Error getting Images");
-    }
-    return res.json();
-  })
-  .then(data=>{
-    let outputData = ``;
-    data.collection.items.map(val=>{
-      // the rendered card
-      outputData += `
-        <div class="column">
-         <img src=${val.links} alt="Hello" />
-        </div>
-      `
-    })
-    output.innerHTML = outputData;
-    console.log(data.collection.items);
-  })
-  .catch(err => alert(err));
-}
+const api_key = "yN0g8ea18vf1HPiUlwOrGB1dgpoyYnJzpIjxXEjm";
+const sendRequest = async () => {
+  const response = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${api_key}&count=50`
+  );
+  if (!response) throw Error("Error getting Images");
+  const data = await response.json();
 
-sendRequest()
+  let outputData = ``;
+
+  data.map((data) =>
+    data.url && data.url.includes(".jpg")
+      ? (outputData += `
+        <div class="column">
+         <img src=${data.url} alt="Hello" />
+        </div>
+  `)
+      : null
+  );
+
+  output.innerHTML = outputData;
+};
+
+sendRequest();
